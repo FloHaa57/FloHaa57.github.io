82e6d10b2fa8d66f7a7aa45e082d32014b212a4f
FloHaa57 <floo.haase@gmail.com>
2017-12-21 12:43:54 +0100

    edited show video script

--- script.js
+++ script.js
@@ -11,9 +11,19 @@ $(document).ready(function() {
 
 		 var video = document.querySelector('video');
 
-		  navigator.mediaDevices.getUserMedia({audio: false, video: true}, function(stream) {
-			video.src = window.URL.createObjectURL(stream);
-		}, errorCallback);
+		//  navigator.mediaDevices.getUserMedia({audio: false, video: true}, function(stream) {
+		//	video.src = window.URL.createObjectURL(stream);
+		//}, errorCallback);
+
+		navigator.getUserMedia('video, audio', function(localMediaStream) {
+	    	video.src = window.URL.createObjectURL(localMediaStream);
+
+	    	// Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
+	    	// See crbug.com/110938.
+	    	video.onloadedmetadata = function(e) {
+	      // Ready to go. Do some stuff.
+	    	};
+	  	}, errorCallback);
 
 	} else {
   		alert('getUserMedia() is not supported in your browser');
@@ -23,4 +33,8 @@ $(document).ready(function() {
 function hasGetUserMedia() {
   return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
             navigator.mozGetUserMedia || navigator.msGetUserMedia);
+}
+
+function errorCallback() {
+	alert("Error loading video...");
 }
